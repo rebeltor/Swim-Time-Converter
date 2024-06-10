a152cfeb7733ef7d9e58a86ca9d96bb7a074b5e6
Tor <tor@aimsparking.com>
2024-06-10 15:54:05 -0400

    Implemented basic time conversions

--- Swim Time Converter/Constants/EventTypes.cs
+++ Swim Time Converter/Constants/EventTypes.cs
@@ -11,20 +11,20 @@ namespace Swim_Time_Converter.Constants
         FREE100,
         [Description("200 Freestyle")]
         FREE200,
-        [Description("50 Butterfly")]
-        FLY50,
+        //[Description("50 Butterfly")]
+        //FLY50,
         [Description("100 Butterfly")]
         FLY100,
         [Description("200 Butterfly")]
         FLY200,
-        [Description("50 Breaststroke")]
-        BREAST50,
+        //[Description("50 Breaststroke")]
+        //BREAST50,
         [Description("100 Breaststroke")]
         BREAST100,
         [Description("200 Breaststroke")]
         BREAST200,
-        [Description("50 Backstroke")]
-        BACK50,
+        //[Description("50 Backstroke")]
+        //BACK50,
         [Description("100 Backstroke")]
         BACK100,
         [Description("200 Backstroke")]
@@ -35,8 +35,8 @@ namespace Swim_Time_Converter.Constants
         IM400,
         [Description("400/500 Freestyle")]
         FREE400_500,
-        [Description("800/100 Freestyle")]
-        FREE800_1000,
+        //[Description("800/100 Freestyle")]
+        //FREE800_1000,
         [Description("1500/1650 Freestyle")]
         FREE1500_1650,
     }
